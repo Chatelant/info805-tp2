e6b4053c1e00e55f25b23a5fbac9030a59cb0580
Elodie D <elodie.deflon@gmail.com>
2022-04-04 16:39:18 +0200

    PAPUSH

--- src/main/java/fr/usmb/m1isc/compilation/tp/AssemblerGenerator.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/AssemblerGenerator.java
@@ -34,35 +34,57 @@ public class AssemblerGenerator {
         String tmp = getCodeSegment(data.getFilsGauche()) + getCodeSegment(data.getFilsDroit());
 
         if (data.getRacine() == Operator.LET) {
-            tmp += String.format("  mov eax, %s\n", data.getFilsDroit());
-            tmp += String.format("  mov %s, eax\n", data.getFilsGauche());
-            tmp += String.format("  mov eax, %s\n", data.getFilsGauche());
-            tmp += "  push eax\n";
-        } else if (data.getRacine() == Operator.PLUS) {
-            tmp += String.format("  mov eax, %s\n", data.getFilsGauche());
-            tmp += "  push eax\n";
-            tmp += String.format("  mov eax, %s\n", data.getFilsDroit());
-            tmp += "  pop ebx\n";
-            tmp += "  add eax, ebx\n";
-        } else if (data.getRacine() == Operator.MOINS) {
-            tmp += String.format("  mov eax, %s\n", data.getFilsGauche());
-            tmp += "  push eax\n";
-            tmp += String.format("  mov eax, %s\n", data.getFilsDroit());
-            tmp += "  pop ebx\n";
-            tmp += "  sub eax, ebx\n";
-        } else if (data.getRacine() == Operator.MUL) {
-            tmp += String.format("  mov eax, %s\n", data.getFilsGauche());
-            tmp += "  push eax\n";
-            tmp += String.format("  mov eax, %s\n", data.getFilsDroit());
-            tmp += "  pop ebx\n";
-            tmp += "  mul eax, ebx\n";
-        } else if (data.getRacine() == Operator.DIV) {
-            tmp += String.format("  mov eax, %s\n", data.getFilsGauche());
-            tmp += "  push eax\n";
-            tmp += String.format("  mov eax, %s\n", data.getFilsDroit());
-            tmp += "  pop ebx\n";
-            tmp += "  div eax, ebx\n";
+            if (data.getFilsDroit().getClass().getSimpleName().equals("Feuille")) {
+                tmp += String.format("    mov eax, %s\n", data.getFilsDroit());
+            } else {
+                String[] lines = tmp.split("\n");
+                boolean isPush = lines[lines.length - 1].contains("push");
+
+                if (isPush) {
+                    tmp = "";
+                    for (int i = 0; i < lines.length - 1; i++) {
+                        tmp += (lines[i] + "\n");
+                    }
+                }
+            }
+            if (data.getFilsGauche().getClass().getSimpleName().equals("Feuille")) {
+                tmp += String.format("    mov %s, eax\n", data.getFilsGauche());
+            }
         }
+
+        if (data.getRacine() == Operator.PLUS || data.getRacine() == Operator.MOINS ||
+            data.getRacine() == Operator.MUL || data.getRacine() == Operator.DIV) {
+
+            // Check la derniere commande
+            String[] lines = tmp.split("\n");
+            boolean isPush = lines[lines.length - 1].contains("push");
+
+            if (data.getFilsGauche().getClass().getSimpleName().equals("Feuille") && !isPush) {
+                tmp += String.format("    mov eax, %s\n", data.getFilsGauche());
+                tmp += "    push eax\n";
+            }
+            if (data.getFilsDroit().getClass().getSimpleName().equals("Feuille")) {
+                tmp += String.format("    mov eax, %s\n", data.getFilsDroit());
+            }
+
+
+            if (data.getRacine() == Operator.PLUS) {
+                tmp += "    pop ebx\n";
+                tmp += "    add eax, ebx\n";
+            } else if (data.getRacine() == Operator.MOINS) {
+                tmp += "    pop ebx\n";
+                tmp += "    sub eax, ebx\n";
+            } else if (data.getRacine() == Operator.MUL) {
+                tmp += "    pop ebx\n";
+                tmp += "    mul eax, ebx\n";
+            } else if (data.getRacine() == Operator.DIV) {
+                tmp += "    pop ebx\n";
+                tmp += "    div ebx, eax\n";
+                tmp += "    mov eax, ebx\n";
+            }
+            tmp += "    push eax\n";
+        }
+
         return tmp;
     }
 
